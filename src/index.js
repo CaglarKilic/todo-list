c9226ac746ae96d44bfb9d7dab63e71c48b6f5c3
Caglar Kilic <73300418+CaglarKilic@users.noreply.github.com>
2023-12-07 12:23:54 +0300

    Ready for dom implementation

--- src/index.js
+++ src/index.js
@@ -4,9 +4,13 @@ import Card from "./card.js";
 
 const Projects = (function () {
   const container = new MyMap();
+  let activeProject = addProject("Inbox");
+
   function addProject(title) {
     const project = new Project(title);
     container.set(project.uid, project);
+    return project;
   }
-  return { container, addProject };
+
+  return { container, addProject, activeProject };
 })();
